4e2805e4e041a6a09c9db7ad1bd39a34e82747e1
Sam Kirsch and Shruti Priya <sam+shruti@>
2017-03-06 10:57:42 -0800

    rename Category to product and recreate product file

--- Objects/Category.cs
+++ Objects/Category.cs
@@ -6,54 +6,24 @@ using System.Data.SqlClient;
 
 namespace OnlineStore.Objects
 {
-    public class Product
+    public class Category
     {
         private int _id;
         private string _name;
-        private int _count;
-        private int _rating;
-        private decimal _price;
-        private string description;
 
-        public Product (string newName, int newCount, int newRating, decimal newPrice, string newDescription, int newId = 0)
+        public Category(string newName, int newId = 0)
         {
             _id = newId;
             _name = newName;
-            _count = newCount;
-            _rating = newRating;
-            _price = newPrice;
-            _description = newDescription;
         }
 
         public int GetId()
         {
             return _id;
         }
-
         public string GetName()
         {
             return _name;
         }
-
-        public int GetCount()
-        {
-            return _count;
-        }
-
-        public int GetRating()
-        {
-            return _rating;
-        }
-
-        public decimal GetPrice()
-        {
-            return _price;
-        }
-
-        public string GetDescription()
-        {
-            return _description;
-        }
-
     }
 }

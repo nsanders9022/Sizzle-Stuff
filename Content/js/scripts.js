328da7da0af13e1dc18ceefce18c496cdd6c7402
Sam Kirsch and Shruti Priya <sam+shruti@>
2017-03-07 19:07:38 -0800

    Add styling to layout mockup

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -9,4 +9,17 @@ $(function(){
          $('#photo-carousel').carousel('next');
     })
 
+    $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the
+      //nav bar to stick.
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 230) {
+      $('#my-nav').addClass('navbar-fixed-top');
+    }
+    if ($(window).scrollTop() < 230) {
+      $('#my-nav').removeClass('navbar-fixed-top');
+    }
+  });
+
 });
